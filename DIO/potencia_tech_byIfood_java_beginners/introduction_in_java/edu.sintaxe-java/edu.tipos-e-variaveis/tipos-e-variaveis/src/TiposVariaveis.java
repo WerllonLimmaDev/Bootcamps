537e20f826da3e263621fcf2c37ec6a77d41b0fd
WerllonLimma <werllonlimma@gmail.com>
2022-11-01 19:24:27 -0300

    Add new aplication.

--- DIO/potencia_tech_byIfood_java_beginners/introduction_in_java/edu.sintaxe-java/edu.tipos-e-variaveis/tipos-e-variaveis/src/TiposVariaveis.java
+++ DIO/potencia_tech_byIfood_java_beginners/introduction_in_java/edu.sintaxe-java/edu.tipos-e-variaveis/tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 12345678;
+        long cpf = 98765432111L;
+        final float VALOR_DE_PI = 3.14F;
+        double salario = 1275.33; 
     }
 }
